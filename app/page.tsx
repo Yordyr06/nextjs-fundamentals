2a5a28c9fae3c82b58b7dc4f965b457c0ab1fc92
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-08-24 21:48:13 -0400

    😳 Eleventh Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "app/components/home/Hero";
 import { Description } from "../components/home/Description";
 import { MainProducts } from "../components/home/MainProducts";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- components/home/Description/index.tsx
+++ components/home/Description/index.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import styles from './styles.module.scss'
+
 export const Description = () =>{
   return (
-    <section>
-      <h1>Description</h1>
+    <section className={styles.Description}>
+      <Image width={500} height={300} src="/images/description.jpeg" alt="" />
+      <div>
+        <h2>Description</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium ullam itaque illum, aspernatur beatae rem! Ex mollitia beatae, quod cumque iure quibusdam doloremque eaque nam repellat hic voluptatum quam deleniti.</p>
+      </div>
     </section>
   )
 }
