a6cbbabe5ada209bcf7cd3186713374b6c7d1f8e
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-08-24 23:15:43 -0400

    😳 Twelfth Commit

--- components/home/Description/index.tsx
+++ components/home/Description/index.tsx
@@ -1,10 +1,20 @@
+import PLACEHOLDER__IMAGE from "./placeholder__image";
 import Image from "next/image";
 import styles from './styles.module.scss'
 
 export const Description = () =>{
   return (
     <section className={styles.Description}>
-      <Image width={500} height={300} src="/images/description.jpeg" alt="" />
+      <div className={styles.Description__imageContainer}>
+        <Image 
+          fill
+          src="/images/description.jpeg"
+          alt="Description Image" 
+          placeholder="blur"
+          blurDataURL={PLACEHOLDER__IMAGE}
+        />
+      </div>
+      
       <div>
         <h2>Description</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium ullam itaque illum, aspernatur beatae rem! Ex mollitia beatae, quod cumque iure quibusdam doloremque eaque nam repellat hic voluptatum quam deleniti.</p>
