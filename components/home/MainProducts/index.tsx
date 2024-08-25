111b7445ce4dc5d3210859a2259dafd745862f5a
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-08-25 11:55:28 -0400

    😳 Fifteenth Commit

--- components/home/MainProducts/index.tsx
+++ components/home/MainProducts/index.tsx
@@ -1,4 +1,18 @@
-export const MainProducts = () =>{
+const getProducts = async () => {
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+    headers: new Headers({
+      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+    })
+  })
+
+  const data = await response.json()
+  return data.products
+}
+
+export const MainProducts = async () =>{
+  const products = await getProducts()
+  console.log(products)
+
   return (
     <section>
       <h1>MainProduct</h1>
